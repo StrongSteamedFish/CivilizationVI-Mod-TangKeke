de9e9d328fad4bc6ff8dff38079f5e27f5fe80a7
StrongSteamedFish <zhengyu_changhe@163.com>
2021-11-09 17:42:51 +0800

    完成了初步框架

---
+++ Mod_Districts.sql
@@ -0,0 +1,79 @@
+-- Mod_Districts
+-- Author: zheng
+-- DateCreated: 11/9/2021 1:21:49 PM
+--------------------------------------------------------------
+INSERT INTO Types
+	(Type, Kind)
+VALUES
+	('DISTRICT_SCHOOL_IDOL_PROJECT', 'KIND_DISTRICT'),
+	('TRAIT_DISTRICT_SCHOOL_IDOL_PROJECT', 'KIND_TRAIT');
+
+INSERT INTO Traits
+	(TraitType, Name, Description)
+VALUES
+	('TRAIT_DISTRICT_SCHOOL_IDOL_PROJECT', 'LOC_TRAIT_DISTRICT_SCHOOL_IDOL_PROJECT_NAME', 'LOC_TRAIT_DISTRICT_SCHOOL_IDOL_PROJECT_DESCRIPTION');
+
+INSERT INTO CivilizationTraits
+	(CivilizationType, TraitType)
+VALUES
+	('CIVILIZATION_LIELLA', 'TRAIT_DISTRICT_SCHOOL_IDOL_PROJECT');
+
+INSERT INTO Districts
+	(DistrictType, Name, PrereqTech, PrereqCivic, Coast, Description, Cost, RequiresPlacement, RequiresPopulation, NoAdjacentCity, CityCenter, Aqueduct, InternalOnly, ZOC, FreeEmbark, HitPoints, CaptureRemovesBuildings, CaptureRemovesCityDefenses, PlunderType, PlunderAmount, TradeEmbark, MilitaryDomain, CostProgressionModel, CostProgressionParam1, TraitType, Appeal, Housing, Entertainment, OnePerCity, AllowsHolyCity, Maintenance, AirSlots, CitizenSlots, TravelTime, CityStrengthModifier, AdjacentToLand, CanAttack, AdvisorType, CaptureRemovesDistrict, MaxPerPlayer)
+SELECT
+	'DISTRICT_SCHOOL_IDOL_PROJECT', 'LOC_DISTRICT_SCHOOL_IDOL_PROJECT_NAME', PrereqTech, PrereqCivic, Coast, 'LOC_DISTRICT_SCHOOL_IDOL_PROJECT_DESCRIPTION', Cost/2, RequiresPlacement, RequiresPopulation, NoAdjacentCity, CityCenter, Aqueduct, InternalOnly, ZOC, FreeEmbark, HitPoints, CaptureRemovesBuildings, CaptureRemovesCityDefenses, PlunderType, PlunderAmount, TradeEmbark, MilitaryDomain, CostProgressionModel, CostProgressionParam1, 'TRAIT_DISTRICT_SCHOOL_IDOL_PROJECT', 2, Housing, 1, OnePerCity, AllowsHolyCity, Maintenance, AirSlots, CitizenSlots, TravelTime, CityStrengthModifier, AdjacentToLand, CanAttack, AdvisorType, CaptureRemovesDistrict, MaxPerPlayer
+FROM Districts WHERE DistrictType='DISTRICT_THEATER';
+
+INSERT INTO District_TradeRouteYields
+	(DistrictType, YieldType, YieldChangeAsOrigin, YieldChangeAsDomesticDestination, YieldChangeAsInternationalDestination)
+SELECT
+	'DISTRICT_SCHOOL_IDOL_PROJECT', YieldType, YieldChangeAsOrigin, YieldChangeAsDomesticDestination, YieldChangeAsInternationalDestination
+FROM District_TradeRouteYields WHERE DistrictType='DISTRICT_THEATER';
+
+INSERT INTO District_GreatPersonPoints
+	(DistrictType, GreatPersonClassType, PointsPerTurn)
+SELECT
+	'DISTRICT_SCHOOL_IDOL_PROJECT', GreatPersonClassType, PointsPerTurn
+FROM District_GreatPersonPoints WHERE DistrictType='DISTRICT_THEATER';
+
+INSERT INTO District_CitizenYieldChanges
+	(DistrictType, YieldType, YieldChange)
+SELECT
+	'DISTRICT_SCHOOL_IDOL_PROJECT', YieldType, YieldChange
+FROM District_CitizenYieldChanges WHERE DistrictType='DISTRICT_THEATER';
+
+INSERT INTO District_Adjacencies
+	(DistrictType, YieldChangeId)
+VALUES
+	('DISTRICT_SCHOOL_IDOL_PROJECT', 'District_School_Idol_Porject_Culture_City_Center'),
+	('DISTRICT_SCHOOL_IDOL_PROJECT', 'District_School_Idol_Porject_Culture_Coast'),
+	('DISTRICT_SCHOOL_IDOL_PROJECT', 'District_School_Idol_Porject_Culture_River');
+
+INSERT INTO District_Adjacencies
+	(DistrictType, YieldChangeId)
+SELECT
+	'DISTRICT_SCHOOL_IDOL_PROJECT', YieldChangeId
+FROM District_Adjacencies WHERE DistrictType='DISTRICT_THEATER';
+
+INSERT INTO Adjacency_YieldChanges
+	(ID, Description, YieldType, YieldChange, TilesRequired, OtherDistrictAdjacent, AdjacentSeaResource, AdjacentTerrain, AdjacentFeature, AdjacentRiver, AdjacentWonder, AdjacentNaturalWonder, AdjacentImprovement, AdjacentDistrict, PrereqCivic, PrereqTech, ObsoleteCivic, ObsoleteTech, AdjacentResource, AdjacentResourceClass, Self)
+SELECT
+	'District_School_Idol_Porject_Culture_City_Center', Description, YieldType, 2, TilesRequired, OtherDistrictAdjacent, AdjacentSeaResource, AdjacentTerrain, AdjacentFeature, AdjacentRiver, AdjacentWonder, AdjacentNaturalWonder, AdjacentImprovement, AdjacentDistrict, PrereqCivic, PrereqTech, ObsoleteCivic, ObsoleteTech, AdjacentResource, AdjacentResourceClass, Self
+FROM Adjacency_YieldChanges WHERE ID='District_Culture_City_Center';
+
+INSERT INTO Adjacency_YieldChanges
+	(ID, Description, YieldType, YieldChange, TilesRequired, OtherDistrictAdjacent, AdjacentSeaResource, AdjacentTerrain, AdjacentFeature, AdjacentRiver, AdjacentWonder, AdjacentNaturalWonder, AdjacentImprovement, AdjacentDistrict, PrereqCivic, PrereqTech, ObsoleteCivic, ObsoleteTech, AdjacentResource, AdjacentResourceClass, Self)
+SELECT
+	'District_School_Idol_Porject_Culture_River', 'LOC_DISTRICT_SCHOOL_IDOL_PORJECT_RIVER_CULTURE', 'YIELD_CULTURE', YieldChange, TilesRequired, OtherDistrictAdjacent, AdjacentSeaResource, AdjacentTerrain, AdjacentFeature, AdjacentRiver, AdjacentWonder, AdjacentNaturalWonder, AdjacentImprovement, AdjacentDistrict, PrereqCivic, PrereqTech, ObsoleteCivic, ObsoleteTech, AdjacentResource, AdjacentResourceClass, Self
+FROM Adjacency_YieldChanges WHERE ID='River_Gold';
+
+INSERT INTO Adjacency_YieldChanges
+	(ID, Description, YieldType, YieldChange, TilesRequired, OtherDistrictAdjacent, AdjacentSeaResource, AdjacentTerrain, AdjacentRiver, AdjacentWonder, AdjacentNaturalWonder, AdjacentResource, AdjacentResourceClass, Self)
+VALUES
+	('District_School_Idol_Porject_Culture_Coast', 'LOC_DISTRICT_SCHOOL_IDOL_PORJECT_COAST_CULTURE', 'YIELD_CULTURE', 2, 1, 0, 0, 'TERRAIN_COAST', 0, 0, 0, 0, 'NO_RESOURCECLASS', 0);
+
+INSERT INTO DistrictReplaces
+	(CivUniqueDistrictType, ReplacesDistrictType)
+VALUES
+	('DISTRICT_SCHOOL_IDOL_PROJECT', 'DISTRICT_THEATER');
+
---
+++ Mod_Units.sql
@@ -0,0 +1,48 @@
+-- Mod_Units
+-- Author: zheng
+-- DateCreated: 11/9/2021 4:09:46 PM
+--------------------------------------------------------------
+INSERT INTO Types
+	(Type, Kind)
+VALUES
+	('UNIT_SCHOOL_IDOL', 'KIND_UNIT'),
+	('TRAIT_UNIT_SCHOOL_IDOL', 'KIND_TRAIT');
+
+INSERT INTO Traits
+	(TraitType, Name, Description)
+VALUES
+	('TRAIT_UNIT_SCHOOL_IDOL', 'LOC_TRAIT_UNIT_SCHOOL_IDOL_NAME', 'LOC_TRAIT_UNIT_SCHOOL_IDOL_DESCRIPTION');
+
+INSERT INTO CivilizationTraits
+	(CivilizationType, TraitType)
+VALUES
+	('CIVILIZATION_LIELLA', 'TRAIT_UNIT_SCHOOL_IDOL');
+
+INSERT INTO UnitReplaces
+	(CivUniqueUnitType, ReplacesUnitType)
+VALUES
+	('UNIT_SCHOOL_IDOL', 'UNIT_ROCK_BAND');
+
+INSERT INTO Units
+	(UnitType, Name, BaseSightRange, BaseMoves, Combat, RangedCombat, Range, Bombard, Domain, FormationClass, Cost, PopulationCost, FoundCity, FoundReligion, MakeTradeRoute, EvangelizeBelief, LaunchInquisition, RequiresInquisition, BuildCharges, ReligiousStrength, ReligionEvictPercent, SpreadCharges, ReligiousHealCharges, ExtractsArtifacts, Description, Flavor, CanCapture, CanRetreatWhenCaptured, TraitType, AllowBarbarians, CostProgressionModel, CostProgressionParam1, PromotionClass, InitialLevel, NumRandomChoices, PrereqTech, PrereqCivic, PrereqDistrict, PrereqPopulation, LeaderType, CanTrain, StrategicResource, PurchaseYield, MustPurchase, Maintenance, Stackable, AirSlots, CanTargetAir, PseudoYieldType, ZoneOfControl, AntiAirCombat, Spy, WMDCapable, ParkCharges, IgnoreMoves, TeamVisibility, ObsoleteTech, ObsoleteCivic, MandatoryObsoleteTech, MandatoryObsoleteCivic, AdvisorType, EnabledByReligion, TrackReligion, DisasterCharges, UseMaxMeleeTrainedStrength, ImmediatelyName, CanEarnExperience)
+SELECT
+	'UNIT_SCHOOL_IDOL', 'LOC_UNIT_SCHOOL_IDOL_NAME', BaseSightRange, BaseMoves, Combat, RangedCombat, Range, Bombard, Domain, FormationClass, 100, PopulationCost, FoundCity, FoundReligion, MakeTradeRoute, EvangelizeBelief, LaunchInquisition, RequiresInquisition, BuildCharges, ReligiousStrength, ReligionEvictPercent, SpreadCharges, ReligiousHealCharges, ExtractsArtifacts, 'LOC_UNIT_SCHOOL_IDOL_DESCRIPTION', Flavor, CanCapture, CanRetreatWhenCaptured, 'TRAIT_UNIT_SCHOOL_IDOL', AllowBarbarians, CostProgressionModel, CostProgressionParam1, PromotionClass, InitialLevel, NumRandomChoices, PrereqTech, 'CIVIC_THE_ENLIGHTENMENT', PrereqDistrict, PrereqPopulation, LeaderType, CanTrain, StrategicResource, PurchaseYield, 0, Maintenance, Stackable, AirSlots, CanTargetAir, PseudoYieldType, ZoneOfControl, AntiAirCombat, Spy, WMDCapable, ParkCharges, IgnoreMoves, TeamVisibility, ObsoleteTech, ObsoleteCivic, MandatoryObsoleteTech, MandatoryObsoleteCivic, AdvisorType, EnabledByReligion, TrackReligion, DisasterCharges, UseMaxMeleeTrainedStrength, ImmediatelyName, CanEarnExperience
+FROM Units WHERE UnitType='UNIT_ROCK_BAND';
+
+INSERT INTO Units_XP2
+	(UnitType, ResourceMaintenanceAmount, ResourceCost, ResourceMaintenanceType, TourismBomb, CanEarnExperience, TourismBombPossible, CanFormMilitaryFormation, MajorCivOnly, CanCauseDisasters, CanSacrificeUnits)
+SELECT
+	'UNIT_SCHOOL_IDOL', ResourceMaintenanceAmount, ResourceCost, ResourceMaintenanceType, TourismBomb, CanEarnExperience, TourismBombPossible, CanFormMilitaryFormation, MajorCivOnly, CanCauseDisasters, CanSacrificeUnits
+FROM Units_XP2 WHERE UnitType='UNIT_ROCK_BAND';
+
+INSERT INTO UnitAiInfos
+	(UnitType, AiType)
+SELECT
+	'UNIT_SCHOOL_IDOL', AiType
+FROM UnitAiInfos WHERE UnitType='UNIT_ROCK_BAND';
+
+INSERT INTO TypeTags
+	(Type, Tag)
+SELECT	
+	'UNIT_ROCK_BAND', Tag
+FROM TypeTags WHERE Type='UNIT_ROCK_BAND';
